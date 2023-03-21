cf04485e0eb7554b8e5f9bcaf557b2ec95ba03ab
白子涵(秋夜Dx <20205900@stu.neu.edu.cn>
2023-03-21 16:16:24 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -3,7 +3,7 @@
 import os
 
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 def fetchAPI(input):
   print("组织语言中, 请稍后...")
